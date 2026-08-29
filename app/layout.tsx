1461abbe0fca75aafbb1b093496b18dafe4ac9f5
03-JS <pyjoso3vitaslim@gmail.com>
2026-08-29 03:50:14 +0200

    First home page iteration Basic visuals done I'd say

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import ColorBends from '@/components/ColorBends';
-import DotGrid from '@/components/DotGrid';
+import Link from "next/link";
 
 const jetbrainsMono = JetBrains_Mono({ subsets: ['latin'], variable: '--font-mono' });
 
@@ -28,8 +28,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-mono", jetbrainsMono.variable)}
     >
-      <body className="min-h-full flex flex-col bg-black text-white">
-        <div className="fixed inset-0 h-screen w-screen overflow-hidden -z-10 blur-2xl">
+      <body className="min-h-full flex flex-col items-center bg-black text-white font-sans">
+        <div className="fixed inset-0 h-screen w-screen overflow-hidden">
           <ColorBends
             colors={["#C9080E", "#C9080E", "#C9080E"]}
             rotation={90}
@@ -46,21 +46,28 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
             transparent
             autoRotate={0}
           />
+          {/* <SideRays
+            speed={2.5}
+            rayColor1="#7C3AED"
+            rayColor2="#C9080E"
+            intensity={2}
+            spread={2}
+            origin="top-left"
+            tilt={0}
+            saturation={1.5}
+            blend={0.75}
+            falloff={1.6}
+            opacity={1}
+          /> */}
         </div>
-        <div className="fixed inset-0 h-screen w-screen overflow-hidden">
-          <DotGrid
-            dotSize={5}
-            gap={15}
-            baseColor="#1c1c1c"
-            activeColor="#C9080E"
-            proximity={120}
-            shockRadius={250}
-            shockStrength={5}
-            resistance={750}
-            returnDuration={1.5}
-          />
-        </div>
-        <main className="z-0">
+        <header className="glass-element border-t-0 border-l-0 border-r-0 rounded-none fixed top-0 w-screen h-16 flex justify-end gap-x-10 items-center pointer-events-none">
+          <Link href="/" className="nav-text">Home</Link>
+          <Link href="/projects" className="nav-text">Projects</Link>
+          <Link href="/contact" className="nav-text">Contact</Link>
+          <Link href="/contact" className="nav-text">About me</Link>
+          <Link href="/support" className="nav-text">Support me</Link>
+        </header>
+        <main className="z-0 min-h-screen flex flex-col justify-center items-center pointer-events-none">
           {children}
         </main>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
 import Image from "next/image";
+import AnimatedContent from "@/components/AnimatedContent";
+import StrokeText from "@/components/StrokeText";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div></div>
+    <>
+      <AnimatedContent className="flex justify-center items-center w-screen p-0" distance={600} direction="vertical" duration={1.15} ease="power3.out" initialOpacity={0} animateOpacity scale={1} threshold={.1} delay={0}>
+        <div className="glass-element pt-12 pb-12 w-fit max-w-3/4 flex flex-row justify-center items-center gap-x-12">
+          <img src="https://avatars.githubusercontent.com/u/107844615?v=4" alt="pfp" className="border-2 border-white/20 shadow-accent rounded-full w-86" />
+          <div className="flex flex-col justify-center">
+            <span className="text-8xl font-bold">Hi! I'm JS03</span>
+            <p className="mt-3 text-xl text-white/80 w-full">
+              Though most people just call me JS.
+            </p>
+            <p className="mt-3 text-xl text-white/80 w-full">
+              I'm a software developer with a passion for gaming, modding and game development. I have been developing mods and creating other related content for games such as <span className="font-bold">Lethal Company</span> and <span className="font-bold">Minecraft</span> since 2019, some of which has been downloaded by hundreds of thousands of users around the world!
+            </p>
+            <p className="mt-3 text-xl text-white/80 w-full">
+              I have also made a couple of games in my free time using the <span className="font-bold">Godot</span> and <span className="font-bold">Unity</span> game engines, which you can check out in the <Link href="/projects" className="pointer-events-auto text-blue-500 font-bold hover:text-[#C9080E] transition-all duration-300">Projects</Link> page.
+            </p>
+            <p className="mt-3 text-xl text-white/80 w-full">
+              And last but not least, I have a <Link href="https://www.youtube.com/@JS03" target="_blank" className="pointer-events-auto text-blue-500 font-bold hover:text-[#C9080E] transition-all duration-300">YouTube channel</Link> with over 2.7k subscribers in which I occasionally upload content related to things I like, such as Minecraft shader comparisons or showcases or DOOM soundtrack game rips that I make in my free time.
+            </p>
+          </div>
+        </div >
+      </AnimatedContent>
+    </>
   );
 }
